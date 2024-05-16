924124d1e5d028d80dbe3eaf53a2205f8b6c7255
Martina Callegaro <marticallegaro29j@gmail.com>
2024-05-16 19:23:35 -0300

    Alumno agregado.

--- ProyectoTransversal1/src/proyectotransversal1/ProyectoTransversal1.java
+++ ProyectoTransversal1/src/proyectotransversal1/ProyectoTransversal1.java
@@ -19,8 +19,11 @@ public class ProyectoTransversal1 {
      */
     public static void main(String[] args) {
         AlumnoData controlAlumno = new AlumnoData();
-        Alumno alumno1 = new Alumno(10,123,"Callegaro","Martina",LocalDate.of(2024,03,29),true);
+        Alumno alumno1 = new Alumno(10,123,"Callegaro","Martina",LocalDate.of(2002,03,29),true);
         controlAlumno.guardarAlumno(alumno1);
+        Alumno alumno2 = new Alumno(11,215,"Maidana","Pablo",LocalDate.of(2001,03,19),true);
+        controlAlumno.guardarAlumno(alumno2);
+        
     }
     
 }
